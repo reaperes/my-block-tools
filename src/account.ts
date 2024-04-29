4a3d8527085bb94707126d68666ee16e111f6bad
reaperes <reaperes105@gmail.com>
2024-04-29 16:39:32 +0900

    update

--- src/account.ts
+++ src/account.ts
@@ -1,6 +1,6 @@
 import { generateMnemonic } from 'ethereum-cryptography/bip39'
 import { wordlist } from 'ethereum-cryptography/bip39/wordlists/english'
 
-export function createMnemonic(): string {
-  return generateMnemonic(wordlist)
+export function createMnemonic(strength?: number): string {
+  return generateMnemonic(wordlist, strength)
 }
